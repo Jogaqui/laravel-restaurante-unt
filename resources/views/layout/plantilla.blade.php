b94e3fffac35af3d10361404a2a92e9d26a237b7
Jogaqui <jogaqui314@gmail.com>
2024-08-20 09:03:48 -0500

    arreglar estilos

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -14,8 +14,9 @@
         <link rel="stylesheet" href="./adminlte/plugins/fontawesome-free/css/all.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="./adminlte/dist/css/adminlte.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href=".\assets\css\bootstrap.min.css">
         @yield('css')
     </head>
 
@@ -455,9 +456,10 @@
         <!-- AdminLTE for demo purposes -->
 
         <!-- Script javascript -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
-        </script>
+        </script> -->
+        <script src=".\assets\js\bootstrap.bundle.min.js"></script>
         @yield('script')
 
     </body>
